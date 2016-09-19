3206b06e5e8b8ddfb7ada3803d5f01f335820239
Lucas Valente <lucas.valente@cpd.ufrgs.br>
2016-09-19 22:56:08 +0000

    Adiciona documentação na classe Permissão

--- ponto/protected/models/Permissao.php
+++ ponto/protected/models/Permissao.php
@@ -1,29 +1,72 @@
 <?php
 
+/**
+ * Modelo criado para representar a tabela permissao
+ * 
+ * Aqui são implementados os métodos básicos do Yii Framework para realizar o 
+ * mapeamento das entidades do banco de dados relacional no paradigma de objetos.
+ * 
+ * Além de tais operações, geralmente são implementados recursos a mais para
+ * reduzir a quantidade de queries e operações repetititvas nos controladores.
+ * 
+ * @property int $id_aplicacao Chave primária da classe Permissao
+ * @property int $id_pessoa Chave primária da classe Pessoa
+ * @property int $id_orgao Chave primária da classe Orgao
+ * @property DateTime $data_expiracao Tempo de validade da permissão gerada
+ * 
+ * @author UFRGS <cpd-dss@ufrgs.br>
+ * @package cpd\spell
+ * @version v1.0
+ * @since v1.0
+ */
 class Permissao extends CActiveRecord {
 
 	/**
-	 * Returns the static model of the specified AR class.
-	 * @return Pessoa the static model class
-	 */
+     * Método do Yii Framework para retornar a instância da classe
+     * 
+     * Esse método deve ser implementado em todas as classe {@see CActiveRecord}
+     * para permitir que o framework encontre a classe.
+     * 
+     * @param string $className Nome da classe que é Active Record.
+     * @return Permissao A classe que é Active Record
+     */
 	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
 
 	/**
-	 * @return string the associated database table name
-	 */
+     * Método do Yii Framework para definição da tabela associada ao objeto
+     * 
+     * A string retornada define para o Yii qual tabela contém os registros a
+     * serem mapeados para essa classe.
+     * 
+     * @return string Nome da tabela no banco de dados associada ao objeto
+     */
 	public function tableName() {
 		return 'permissao';
 	}
 
+    /**
+     * Método do Yii Framework para definição da chave primária do objeto
+     * 
+     * A string retornada indica a coluna contendo o identificador único do 
+     * objetos.
+     * 
+     * @return string Nome da coluna referente à chave primária do objeto
+     */
 	public function primaryKey() {
 		return array('id_aplicacao', 'id_pessoa');
 	}
 
 	/**
-	 * @return array relational rules.
-	 */
+     * Método do Yii Framework para definição de relacionamentos entre tabelas
+     * 
+     * Aqui são definidos as tabelas, os tipos de relação e as colunas que as 
+     * possuem.
+     * 
+     * @link http://www.yiiframework.com/doc/guide/1.1/en/database.arr#declaring-relationship Como declarar relacionamentos
+     * @return array Relacionamentos que esta tabela possui
+     */
 	public function relations() {
 		return array(
             'Pessoa' => array(self::BELONGS_TO, 'Pessoa', 'id_pessoa'),
