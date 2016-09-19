fbf1f22f1acef4a6a54df2d69925e1adc50130c9
Lucas Valente <lucas.valente@cpd.ufrgs.br>
2016-09-19 22:11:36 +0000

    Adiciona documentação na classe DadoFuncional

--- ponto/protected/models/DadoFuncional.php
+++ ponto/protected/models/DadoFuncional.php
@@ -1,23 +1,83 @@
 <?php
 
+/**
+ * Modelo criado para representar a tabela dado_funcional
+ * 
+ * Aqui são implementados os métodos básicos do Yii Framework para realizar o 
+ * mapeamento das entidades do banco de dados relacional no paradigma de objetos.
+ * 
+ * Além de tais operações, geralmente são implementados recursos a mais para
+ * reduzir a quantidade de queries e operações repetititvas nos controladores.
+ * 
+ * @property int $matricula Chave primária da classe DadoFuncional
+ * @property int $nr_vinculo Chave primária da classe DadoFuncional
+ * @property int $id_pessoa Chave primária da classe Pessoa
+ * @property char $regime_trabalho Tempo de trabalho dessa categoria (Ex: 40)
+ * @property int $id_grupo Chave primária da classe GrupoEmprego
+ * @property int $id_categoria Chave primária da classe CategoriaFuncional
+ * @property int $orgao_lotacao 
+ * @property int $orgao_exercicio Chave primária do órgão o qual o funcionário atende
+ * @property DateTime $data_ingresso Data em que a pessoa ingressou no cargo
+ * @property DateTime $data_desligamento Data em que a pessoa se desligou do cargo
+ * @property DateTime $data_aposentadoria Data em que a pessoa se aposentou do cargo
+ * 
+ * @author UFRGS <cpd-dss@ufrgs.br>
+ * @package cpd\spell
+ * @version v1.0
+ * @since v1.0
+ */
 class DadoFuncional extends CActiveRecord
 {
 
+    /**
+     * Método do Yii Framework para retornar a instância da classe
+     * 
+     * Esse método deve ser implementado em todas as classe {@see CActiveRecord}
+     * para permitir que o framework encontre a classe.
+     * 
+     * @param string $className Nome da classe que é Active Record.
+     * @return DadoFuncional A classe que é Active Record
+     */
     public static function model($className = __CLASS__)
     {
         return parent::model($className);
     }
 
+    /**
+     * Método do Yii Framework para definição da tabela associada ao objeto
+     * 
+     * A string retornada define para o Yii qual tabela contém os registros a
+     * serem mapeados para essa classe.
+     * 
+     * @return string Nome da tabela no banco de dados associada ao objeto
+     */
     public function tableName()
     {
         return 'dado_funcional';
     }
 
+    /**
+     * Método do Yii Framework para definição da chave primária do objeto
+     * 
+     * A string retornada indica a coluna contendo o identificador único do 
+     * objetos.
+     * 
+     * @return string Nome da coluna referente à chave primária do objeto
+     */
     public function primaryKey()
     {
         return array('matricula', 'nr_vinculo');
     }
   
+    /**
+     * Método do Yii Framework para definição de relacionamentos entre tabelas
+     * 
+     * Aqui são definidos as tabelas, os tipos de relação e as colunas que as 
+     * possuem.
+     * 
+     * @link http://www.yiiframework.com/doc/guide/1.1/en/database.arr#declaring-relationship Como declarar relacionamentos
+     * @return array Relacionamentos que esta tabela possui
+     */
     public function relations()
     {
         return array(
