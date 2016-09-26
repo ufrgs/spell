96b25b9624f1667ed0631cc7ea8f8bf9ceb83ba3
Lucas Valente <lucas.valente@cpd.ufrgs.br>
2016-09-26 20:45:28 +0000

    Adiciona documentação do arquivo pedidosAvaliacao.js

--- ponto/js/pedidosAvaliacao.js
+++ ponto/js/pedidosAvaliacao.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
     $("#modal").dialog({title: "Informações do Pedido", autoOpen: false, width: 600, height: 450});
 });
 
+/**
+ * **pedidosAvaliacao.js**
+ * 
+ * Função utilizada para mostrar detalhes de um pedido de ajuste. Quando chamada
+ * um modal é aberto com as informações do pedido.
+ * 
+ * @param {int} nr Número do pedido a ser excluido
+ * @param {string} tipo Tipo do pedido. Na tela de ajustes tem o valor "ajuste"
+ * @returns {void} Mostra um modal com o pedido solicitado ou uma mensagem de erro
+ */
 function verPedido(nr, tipo) {
     $.ajax({
         url: HOME+"ajuste/dadosPedido",
@@ -18,6 +28,16 @@ function verPedido(nr, tipo) {
     });
 }
 
+/**
+ * **pedidosAvaliacao.js**
+ * 
+ * Método utilizada para certificar um pedido de ajuste. Ao ser chamada, o método
+ * faz uma requisição ao servidor contendo o tipo de pedido e o estado dele.
+ * 
+ * @param {char} certifica S ou N indicando a aprovação ou reprovação do pedido de ajuste
+ * @param {String} tipo Tipo do ajuste solicitado.
+ * @returns {void} Mostra uma mensagem de sucesso ou erro após a tentativa de certificação
+ */
 function certificarPedido(certifica, tipo) {
     if ($("#nrAjuste").val() != '') {
         if ((certifica == 'N') && ($("#justificativa").val().length < 3)) {
@@ -38,7 +58,7 @@ function certificarPedido(certifica, tipo) {
                 success: function(retorno) {
                     $("#modal").html('<fieldset class="field'+(retorno.erro ? 'Erro' : 'Sucesso')+'">'+retorno.mensagem+'</fieldset>');
                     if (retorno.mensagem.indexOf("sucesso") != -1) {
-                        setTimeout(function(){window.location=HOME+"ajuste/pedidosAvaliacao/"}, 2000);
+                        setTimeout(function(){window.location=HOME+"ajuste/pedidosAvaliacao.js/"}, 2000);
                     }
                 },
                 error: function(retorno) {
@@ -49,6 +69,16 @@ function certificarPedido(certifica, tipo) {
     }
 }
 
+/**
+ * **pedidosAvaliacao.js**
+ * 
+ * Função utilizada para certificar vários pedidos de uma vez só. Ao ser chamada, 
+ * o método faz uma requisição ao servidor contendo os tipos e estados de todos
+ * os pedidos ainda não certificados que estejam visiveis na tela do usuário.
+ * 
+ * @param {String} tipo Tipo do ajuste solicitado.
+ * @returns {void} Mostra uma mensagem de sucesso ou erro após a tentativa de certificação
+ */
 function certificarSelecionados(tipo) {
     var pedidos = $("input[name=certificar"+tipo+"]:checked");
     if (pedidos.length > 0) {
@@ -68,7 +98,7 @@ function certificarSelecionados(tipo) {
                 $("#modal").html('<fieldset class="field'+(retorno.erro ? 'Erro' : 'Sucesso')+'">'+retorno.mensagem+'</fieldset>');
                 $("#modal").dialog('open');
                 if (retorno.mensagem.indexOf("sucesso") != -1) {
-                    setTimeout(function(){window.location=HOME+"ajuste/pedidosAvaliacao/"}, 2000);
+                    setTimeout(function(){window.location=HOME+"ajuste/pedidosAvaliacao.js/"}, 2000);
                 }
             },
             error: function(retorno) {
