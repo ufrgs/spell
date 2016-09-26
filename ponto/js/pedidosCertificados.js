0f61393b9f17db637e4615378bde99f413020ef6
Lucas Valente <lucas.valente@cpd.ufrgs.br>
2016-09-26 20:48:22 +0000

    Adiciona documentação no arquivo pedidosCertificados.js

--- ponto/js/pedidosCertificados.js
+++ ponto/js/pedidosCertificados.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
     $("#modal").dialog({title: "Informações do Pedido", autoOpen: false, width: 600, height: 450});
 });
 
+/**
+ * **pedidosCertificados.js**
+ * 
+ * Função utilizada para mostrar detalhes de um pedido de ajuste já certificado. 
+ * Quando chamada um modal é aberto com as informações do pedido.
+ * 
+ * @param {int} nr Número do pedido a ser excluido
+ * @param {string} tipo Tipo do pedido. Na tela de ajustes tem o valor "ajuste"
+ * @returns {void} Mostra um modal com o pedido solicitado ou uma mensagem de erro
+ */
 function verPedido(nr, tipo) {
     $.ajax({
         url: HOME+"ajuste/dadosPedidoCertificado",
