7069201158a7da1f2cbf344cb7e0fe1ba183369d
thiago <thiago@cpd.ufrgs.br>
2016-09-30 14:44:09 -0300

    Bug fix #23

--- ponto/js/pedidosAvaliacao.js
+++ ponto/js/pedidosAvaliacao.js
@@ -98,7 +98,7 @@ function certificarSelecionados(tipo) {
                 $("#modal").html('<fieldset class="field'+(retorno.erro ? 'Erro' : 'Sucesso')+'">'+retorno.mensagem+'</fieldset>');
                 $("#modal").dialog('open');
                 if (retorno.mensagem.indexOf("sucesso") != -1) {
-                    setTimeout(function(){window.location=HOME+"ajuste/pedidosAvaliacao.js/"}, 2000);
+                    setTimeout(function(){window.location=HOME+"ajuste/pedidosAvaliacao/"}, 2000);
                 }
             },
             error: function(retorno) {
